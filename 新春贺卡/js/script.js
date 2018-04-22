fcf99d66dbc266aead7986c3807216e93fab72b8
周如生 <nfuzrs@126.com>
2018-04-22 19:58:58 +0800

    控制音乐播放

---
+++ 新春贺卡/js/script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	var music = document.getElementById('music');
+	var audio = document.getElementsByTagName('audio')[0];
+
+    music.onclick = function() {
+    	if (audio.paused) {
+            audio.play();
+            this.setAttribute("class","play");
+            // this.style.animationPlayState = "running";
+    	} else {
+	       	audio.pause();
+	       	this.setAttribute("class"," ");
+	       	// this.style.animationPlayState = "paused";
+	    }
+    }
+}
