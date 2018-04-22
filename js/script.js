645a30f572d22a77de5a78a59ea658f16c8ae83d
周如生 <nfuzrs@126.com>
2018-04-22 20:19:33 +0800

    touch暂停音乐

--- js/script.js
+++ js/script.js
@@ -2,15 +2,39 @@ window.onload = function() {
 	var music = document.getElementById('music');
 	var audio = document.getElementsByTagName('audio')[0];
 
-    music.onclick = function() {
-    	if (audio.paused) {
+
+    // 当音乐播放结束，光盘旋转效果自动停止
+    audio.addEventListener("ended",function(event){
+        music.setAttribute("class","");
+    },false);
+
+    // 点击音乐图标，控制音乐播放,方法一：click事件
+    // music.onclick = function() {
+    // 	if (audio.paused) {
+    //         audio.play();
+    //         // this.setAttribute("class","play");
+    //         this.style.animationPlayState = "running";
+    //         this.style.webkitAnimationPlayState = "running";
+    // 	} else {
+	   //     	audio.pause();
+	   //     	// this.setAttribute("class"," ");
+    //         this.style.animationPlayState = "paused";
+	   //     	this.style.webkitAnimationPlayState = "paused";
+	   //  }
+    // }
+    
+    // 点击音乐图标，控制音乐播放,方法二：touch事件
+    music.addEventListener("touchstart",function(event){
+        if (audio.paused) {
             audio.play();
             // this.setAttribute("class","play");
             this.style.animationPlayState = "running";
-    	} else {
-	       	audio.pause();
-	       	// this.setAttribute("class"," ");
-	       	this.style.animationPlayState = "paused";
-	    }
-    }
+            this.style.webkitAnimationPlayState = "running";
+        } else {
+            audio.pause();
+            // this.setAttribute("class"," ");
+            this.style.animationPlayState = "paused";
+            this.style.webkitAnimationPlayState = "paused";
+        }
+    },false);
 }
