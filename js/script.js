a504f255f1c7228bbfa2d6d345d4bd1eb63d0717
周如生 <nfuzrs@126.com>
2018-04-22 20:43:48 +0800

    翻页效果

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
 	var music = document.getElementById('music');
 	var audio = document.getElementsByTagName('audio')[0];
+    var page1 = document.getElementById('page1');
+    var page2 = document.getElementById('page2');
+    var page3 = document.getElementById('page3');
 
 
     // 当音乐播放结束，光盘旋转效果自动停止
@@ -37,4 +40,17 @@ window.onload = function() {
             this.style.webkitAnimationPlayState = "paused";
         }
     },false);
-}
+
+    // 页面跳转
+     page1.addEventListener("touchstart",function(event){
+        page1.style.display = "none";
+        page2.style.display = "block";
+        page3.style.display = "block";
+        page3.style.top     = "100%";
+
+        setTimeout(function(){
+            page2.setAttribute("class","page fadeOut");
+            page3.setAttribute("class","page fadeIn");
+        },5500);
+      },false);
+};
