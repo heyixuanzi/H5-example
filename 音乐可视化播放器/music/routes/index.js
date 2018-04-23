5cf26c907494a1ce5cb5c59c9e2d4503085c6674
周如生 <nfuzrs@126.com>
2018-04-23 21:25:00 +0800

    routes路由

--- 音乐可视化播放器/music/routes/index.js
+++ 音乐可视化播放器/music/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mediaPath = 'public/media';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function(req, res){
+	var fs = require("fs");
+	fs.readdir(mediaPath, function(err, files){
+		if(err){
+			console.log(err);
+		}else{
+			res.render('index', {title: 'passionate music', music: files});
+		}
+	});
 });
 
 module.exports = router;
