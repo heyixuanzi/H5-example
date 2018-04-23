7c2c557df1c59a1bc2be54e90ed77be51124c34a
周如生 <nfuzrs@126.com>
2018-04-23 22:11:21 +0800

    音乐列表绑定

--- 音乐可视化播放器/music/routes/index.js
+++ 音乐可视化播放器/music/routes/index.js
@@ -5,10 +5,12 @@ var mediaPath = 'public/media';
 
 router.get('/', function(req, res){
 	var fs = require("fs");
+	// readdir 读取目录中文件名
 	fs.readdir(mediaPath, function(err, files){
 		if(err){
 			console.log(err);
 		}else{
+			// 数据读取成功返回给前端
 			res.render('index', {title: 'passionate music', music: files});
 		}
 	});
