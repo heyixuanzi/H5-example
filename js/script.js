f29f28180f6013e0dcfe4fdf75f0f517830887c4
周如生 <nfuzrs@126.com>
2018-05-01 23:51:41 +0800

    test

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-window.onload = function() {
-	var music = document.getElementById('music');
-	var audio = document.getElementsByTagName('audio')[0];
+window.onload = () => {
+    const page1 = document.getElementById('page1');
+    const page2 = document.getElementById('page2');
+    const page3 = document.getElementById('page3');
+    const music = document.getElementById('music');
+    const audio = document.getElementsByTagName('audio')[0];
 
-    music.onclick = function() {
-    	if (audio.paused) {
+    audio.addEventListener('ended', () => {
+        music.style.animationPlayState="paused";
+        music.style.webkitAnimationPlayState="paused";
+    });
+    music.addEventListener('touchstart', () => {
+        if(audio.paused){
             audio.play();
-            this.setAttribute("class","play");
-            // this.style.animationPlayState = "running";
-    	} else {
-	       	audio.pause();
-	       	this.setAttribute("class"," ");
-	       	// this.style.animationPlayState = "paused";
-	    }
-    }
+            music.style.animationPlayState="running";
+            music.style.webkitAnimationPlayState="running";
+        }else{
+            audio.pause();
+            music.style.animationPlayState="paused";
+            music.style.webkitAnimationPlayState="paused";
+        }
+    },false);
+
+    page1.addEventListener('touchstart', () => {
+        page1.style.display = 'none';
+        page2.style.display = 'block';
+        page3.style.display = 'block';
+        page3.style.top       = '100%'; 
+
+        setTimeout(() => {
+            page2.setAttribute('class','page fadeOut');
+            page3.setAttribute('class','page fadeIn');
+        },5500)
+    },false);
 }
